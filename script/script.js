748f49f2f87a3a523d01aa8b3d12e531ef489684
davidvanr21 <50579850+davidvanr21@users.noreply.github.com>
2023-05-16 23:51:02 +0200

    images compressed, credits of artists added and some more

--- script/script.js
+++ script/script.js
@@ -1,18 +1,17 @@
-// Object met toetsen en afbeeldingen
 var toetsAfbeeldingen = {
   'a': 'images/afbeelding_a.png',
   'b': 'images/afbeelding_b.png',
   'c': 'images/afbeelding_c.png',
-  'd': 'images/afbeelding_d.png',
+  'd': 'images/afbeelding_d.gif',
   'e': 'images/afbeelding_e.png',
   'f': 'images/afbeelding_f.png',
   'g': 'images/afbeelding_g.png',
-  'h': 'images/afbeelding_h.png',
+  'h': 'images/afbeelding_h.gif',
   'i': 'images/afbeelding_i.png',
   'j': 'images/afbeelding_j.png',
   'k': 'images/afbeelding_k.png',
   'l': 'images/afbeelding_l.png',
-  'm': 'images/afbeelding_m.png',
+  'm': 'images/afbeelding_m.gif',
   'n': 'images/afbeelding_n.png',
   'o': 'images/afbeelding_o.png',
   'p': 'images/afbeelding_p.png',
@@ -36,12 +35,12 @@ var toetsAfbeeldingen = {
   '7': 'images/afbeelding_7.png',
   '8': 'images/afbeelding_8.png',
   '9': 'images/afbeelding_9.png',
-  // Voeg hier meer toetsen en hun respectievelijke afbeeldingen toe
 };
 
-// Functie om afbeelding te wijzigen en een random positie te geven
+// Functie om afbeelding te wijzigen en een willekeurige positie te geven
 function wijzigAfbeelding(toets) {
   var afbeeldingElement = document.getElementById('afbeelding');
+  var howtoElement = document.getElementById('howto');
 
   if (toets in toetsAfbeeldingen) {
     var afbeelding = toetsAfbeeldingen[toets];
@@ -54,6 +53,9 @@ function wijzigAfbeelding(toets) {
     // Positie toepassen op de afbeelding
     afbeeldingElement.style.left = positieX + 'px';
     afbeeldingElement.style.top = positieY + 'px';
+
+    afbeeldingElement.style.display = 'block';
+    howtoElement.style.display = 'none';
   }
 }
 
@@ -64,22 +66,26 @@ document.addEventListener('keydown', function(event) {
 });
 
 
-
-// random position, sound, fade out, same size
-
-
-
+// OPTELLEN van 0 naar 36
 document.addEventListener("DOMContentLoaded", function() {
   var counterElement = document.getElementById("loading-counter");
   var counter = 0;
   var loadingScreen = document.getElementById("loading-screen");
   var loadingScreenHeight = loadingScreen.offsetHeight;
+  var animationDuration = 3000; // Duur van de animatie in milliseconden
+  var startTime = null;
 
-  function updateCounter() {
-    counterElement.textContent = counter;
+  function updateCounter(timestamp) {
+    if (!startTime) startTime = timestamp;
+    var progress = timestamp - startTime;
+    var easeProgress = easeInOut(progress, 0, 36, animationDuration); // Pas de getalbereiken en animatieduur aan indien nodig
 
-    if (counter === 36) {
-      // Verberg het laadscherm wanneer de teller 36 bereikt
+    counterElement.textContent = Math.round(easeProgress);
+
+    if (progress < animationDuration) {
+      requestAnimationFrame(updateCounter);
+    } else {
+      // Verberg het laadscherm wanneer de animatie is voltooid
       setTimeout(function() {
         var animationEndHeight = -loadingScreenHeight + "px";
         loadingScreen.style.transform = "translateY(" + animationEndHeight + ")";
@@ -87,11 +93,147 @@ document.addEventListener("DOMContentLoaded", function() {
           loadingScreen.style.display = "none";
         }, 2000);
       }, 1000);
-    } else {
-      counter++;
-      setTimeout(updateCounter, 60); // Verhoog de teller elke 100 milliseconden
     }
   }
 
-  updateCounter();
+  function easeInOut(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t * t + b;
+    t -= 2;
+    return c / 2 * (t * t * t + 2) + b;
+  }
+
+  requestAnimationFrame(updateCounter);
 });
+
+
+
+
+// BROWSER SIZES
+    // Functie om de breedte en hoogte van de browser weer te geven
+    function toonBrowserAfmetingen() {
+      var breedte = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      var hoogte = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+      document.getElementById("browser-breedte").textContent = breedte;
+      document.getElementById("browser-hoogte").textContent = hoogte;
+    }
+
+    // Roep de functie aan wanneer de pagina laadt
+    window.onload = function() {
+      toonBrowserAfmetingen();
+    };
+
+    // Roep de functie aan wanneer het venster van grootte verandert
+    window.onresize = function() {
+      toonBrowserAfmetingen();
+    };
+
+
+
+
+    // CLICKED LETTERS
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+var letters = document.getElementsByClassName("letter");
+
+document.addEventListener("keydown", function(event) {
+  var key = event.key.toUpperCase();
+
+  if (alphabet.includes(key)) {
+    var letterElement = document.getElementById(key);
+
+    if (letterElement) {
+      letterElement.classList.add("clicked");
+    }
+  }
+});
+
+for (var i = 0; i < letters.length; i++) {
+  letters[i].id = letters[i].textContent;
+}
+
+
+
+
+
+// ARISTS NAMES
+var resultElement = document.getElementById("result");
+
+    document.addEventListener("keydown", function(event) {
+      var key = event.key.toUpperCase();
+
+      if (key === "A" || key === "F" || key === "I" || key === "J" || key === "S" || key === "T" || key === "0" || key === "2"|| key === "6") {
+        resultElement.textContent = "by Stef Paul";
+      }
+    });
+
+    document.addEventListener("keydown", function(event) {
+      var key = event.key.toUpperCase();
+
+      if (key === "B" || key === "H" || key === "O" || key === "Y" || key === "3" || key === "7" || key === "9") {
+        resultElement.textContent = "by Mark Vogelaar";
+      }
+    });
+
+    document.addEventListener("keydown", function(event) {
+      var key = event.key.toUpperCase();
+
+      if (key === "C" || key === "L" || key === "N" || key === "U" || key === "1") {
+        resultElement.textContent = "by Robert Buurman";
+      }
+    });
+
+    document.addEventListener("keydown", function(event) {
+      var key = event.key.toUpperCase();
+
+      if (key === "D" || key === "P" || key === "V" || key === "Z" || key === "5") {
+        resultElement.textContent = "by Appie";
+      }
+    });
+
+    document.addEventListener("keydown", function(event) {
+      var key = event.key.toUpperCase();
+
+      if (key === "E" || key === "G" || key === "Q" || key === "W") {
+        resultElement.textContent = "by Mark van der Veen";
+      }
+    });
+
+    document.addEventListener("keydown", function(event) {
+      var key = event.key.toUpperCase();
+
+      if (key === "K") {
+        resultElement.textContent = "by Emil";
+      }
+    });
+
+    document.addEventListener("keydown", function(event) {
+      var key = event.key.toUpperCase();
+
+      if (key === "M") {
+        resultElement.textContent = "by Marleen van den Brand";
+      }
+    });
+
+    document.addEventListener("keydown", function(event) {
+      var key = event.key.toUpperCase();
+
+      if (key === "R") {
+        resultElement.textContent = "by Femke van Zeventer";
+      }
+    });
+
+    document.addEventListener("keydown", function(event) {
+      var key = event.key.toUpperCase();
+
+      if (key === "X") {
+        resultElement.textContent = "by Madelein Nienkemper";
+      }
+    });
+
+    document.addEventListener("keydown", function(event) {
+      var key = event.key.toUpperCase();
+
+      if (key === "4" || key === "8") {
+        resultElement.textContent = "by Miriam Jimenez Lluva";
+      }
+    });
